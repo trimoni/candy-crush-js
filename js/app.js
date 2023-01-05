5b0d7dcf5ba56a1c00274f5d752dc417f88eda95
Tal <trimoni8@gmail.com>
2023-01-05 13:24:12 -0500

    for loop row column

--- js/app.js
+++ js/app.js
@@ -3,3 +3,15 @@ let board = []
 let row = 9
 let columns = 9
 let score = 0
+
+window.onload = function() {
+  startGame()
+}
+
+function startGame() {
+  for (let r = 0; r < rows; r++){
+    for (let c = 0; c < columns; c++){
+      
+    }
+  }
+}
