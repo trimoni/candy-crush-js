55b4e3d189ecaa99f205b5c5214800a81477e360
Tal <trimoni8@gmail.com>
2023-01-05 13:48:21 -0500

    fix rows

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let candies = ['Blue', 'Orange', 'Green', 'Yellow', 'Red', 'Purple']
 let board = []
-let row = 9
+let rows = 9
 let columns = 9
 let score = 0
 
@@ -9,16 +9,17 @@ window.onload = function() {
 }
 
 function randomCandy() {
-  return candies[Math.floor(Math.random() * candies.length)]
+  return candies[Math.floor(Math.random() * candies.length)]; //0 - 5.99
 }
 
 function startGame() {
-  for (let r = 0; r < rows; r++){
-    let row = []
-    for (let c = 0; c < columns; c++){
-      let tile = document.createElement('img')
-      tile.id = r.toString() + '-' + c.toString()
-      tile.src = './images/' + randomCandy() + '.png'
+  for (let r = 0; r < rows; r++) {
+      let row = [];
+      for (let c = 0; c < columns; c++) {
+          // <img id="0-0" src="./images/Red.png">
+          let tile = document.createElement("img");
+          tile.id = r.toString() + "-" + c.toString();
+          tile.src = './images/' + randomCandy() + ".png";
 
       document.getElementById('board').append(tile)
       row.push(tile)
