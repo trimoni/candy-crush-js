b7e256dd2cd9b7aef0214542ab2159fcd7bd2a7d
Tal <trimoni8@gmail.com>
2023-01-05 13:21:57 -0500

    candie variable

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
-// A = 0
-// A(0) = 1
-// A(1) = 32767
-// A(100) = 'Ninety-nine'
-// A(32767) = 'string'
-
-// console.log(A(A(A+1)))
-
-console.log()
+let candies = ['Blue', 'Orange', 'Green', 'Yellow', 'Red', 'Purple']
