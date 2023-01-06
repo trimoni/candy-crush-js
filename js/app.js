72e2727f35a65ea97650029ca95b07264678232f
Tal <trimoni8@gmail.com>
2023-01-06 09:24:32 -0500

    generate candy implement

--- js/app.js
+++ js/app.js
@@ -176,5 +176,9 @@ function slideCandy(){
 }
 
 function generateCandy() {
-
+  for (let c = 0; c < columns; c++){
+    if (board[0][c].src.includes('blank')){
+      board[0][c].src = "./images/" + randomCandy() + '.png'
+    }
+  }
 }
